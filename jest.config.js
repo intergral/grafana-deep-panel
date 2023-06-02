2f09fc6e3f2f5553d90f7d42a7707318d145e87f
Ben Donnelly <ben_donnelly@intergral.com>
2023-06-02 11:13:09 +0100

    chore(license): update license to agpl 3

--- jest.config.js
+++ jest.config.js
@@ -1,3 +1,20 @@
+/*
+ *     Copyright (C) 2023  Intergral GmbH
+ *
+ *     This program is free software: you can redistribute it and/or modify
+ *     it under the terms of the GNU Affero General Public License as published by
+ *     the Free Software Foundation, either version 3 of the License, or
+ *     (at your option) any later version.
+ *
+ *     This program is distributed in the hope that it will be useful,
+ *     but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *     GNU Affero General Public License for more details.
+ *
+ *     You should have received a copy of the GNU Affero General Public License
+ *     along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
 // force timezone to UTC to allow tests to work regardless of local timezone
 // generally used by snapshots, but can affect specific tests
 process.env.TZ = 'UTC';
--- src/README.md
+++ src/README.md
@@ -2,4 +2,6 @@
 
 # Grafana Deep Panel
 
-Panel to render snapshot data
+This is a panel to render snapshot data which is collected by [DEEP](https://github.com/intergral/deep). 
+
+This plugin is best used with the Deep Datasource Plugin.
